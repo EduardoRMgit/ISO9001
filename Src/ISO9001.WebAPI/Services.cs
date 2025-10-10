31ca943ca670a25c5bdfc0b4502366749d0bda0e
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2025-10-09 20:32:57 -0600

    Se Realizar AuditLog

--- Src/ISO9001.WebAPI/Services.cs
+++ Src/ISO9001.WebAPI/Services.cs
@@ -1,14 +1,10 @@
 ﻿using ISO9001.AuditEvents.Repositories;
 using ISO9001.Database.InMemory;
 using ISO9001.GenerateAuditReport.IoC;
-using ISO9001.GetAllAuditLogs.IoC;
 using ISO9001.GetAllCustomerFeedback.IoC;
 using ISO9001.GetAllIncidentReports.IoC;
 using ISO9001.GetAllNonConformities.IoC;
 using ISO9001.GetAuditEvents.IoC;
-using ISO9001.GetAuditLogById.IoC;
-using ISO9001.GetAuditLogsByAction.IoC;
-using ISO9001.GetAuditLogsByEntityId.IoC;
 using ISO9001.GetCustomerFeedbackByCustomerId.IoC;
 using ISO9001.GetCustomerFeedbackByEntityId.IoC;
 using ISO9001.GetCustomerFeedbackById.IoC;
@@ -18,12 +14,12 @@ using ISO9001.GetNonConformityByAffectedProcess.IoC;
 using ISO9001.GetNonConformityByEntityId.IoC;
 using ISO9001.GetNonConformityByStatus.IoC;
 using ISO9001.GetQualityDashBoard.IoC;
-using ISO9001.RegisterAuditLog.IoC;
 using ISO9001.RegisterCustomerFeedback.IoC;
 using ISO9001.RegisterIncidentReportIoC;
 using ISO9001.RegisterNonConformityDetail.IoC;
 using ISO9001.RegisterNonConformityIoC;
 using ISO9001.Repositories;
+using ISO9001.AuditLog.Core;
 
 namespace ISO9001.WebAPI
 {
@@ -31,11 +27,7 @@ namespace ISO9001.WebAPI
     {
         public static WebApplicationBuilder AddISO9001Services(this WebApplicationBuilder builder)
         {
-            builder.Services.AddGetAllAuditLogsServices();
-            builder.Services.AddGetAuditLogByIdServices();
-            builder.Services.AddGetAuditLogsByEntityIdServices();
-            builder.Services.AddGetAuditLogsByActionServices();
-            builder.Services.AddRegisterAuditLogServices();
+            builder.Services.AddAuditLogCoreServices();
 
             builder.Services.AddRegisterCustomerFeedbackServices();
             builder.Services.AddGetAllCustomerFeedbackServices();
